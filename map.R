821c5027191cf7eac1a502e4c3e924f5b6d5cc93
William <william@rodriguezfamily.tv>
2017-12-02 12:50:46 -0800

    Remove extra meteor data. Switched map.R to use correct data

--- map.R
+++ map.R
@@ -1,18 +1,11 @@
+#required libraries
 library(plotly)
 library(dplyr)
 library(tidyr)
 
-setwd("~/Desktop/meteor")
-data <- read.csv("./data/meteor.csv")
+#setwd("~/Desktop/meteor")
+data <- read.csv("./data/meteorite-landings.csv")
 
-# Change the format of the date to be month/day/year.
-data$year <- format(as.Date(data$year), "%m/%d/%Y")
-
-# Separate the geoLocation to two columns Longitude, Latitude.
-meteor.data <- data %>%
-  extract(GeoLocation, c("Latitude", "Longitude"), "\\(([^,]+), ([^)]+)\\)")
-
-# Create a map that displays the location of the meteor landings.
 g <- list(
   scope = 'world',
   showland = TRUE,
@@ -30,8 +23,7 @@ g <- list(
 
 map <- plot_geo(meteor.data) %>%  
   layout(title = "Where Meteor's Land", geo = g) %>%  
-  add_markers(x = ~Longitude, y = ~Latitude, hoverinfo = "text",
+  add_markers(x = ~reclong, y = ~reclat, hoverinfo = "text",
               text = ~paste("Date: ", year, "</br></br>", "Name of Meteor: ", name, "</br>Size: ", mass..g.),
               marker = list(color = 'rgb(126, 41, 162)')
-  )  
-map
+  )
