bdef6d0dd2d0a21ca7fc0ba9bb7db34c214c264c
Justin Park <justinjpark98@gmail.com>
2017-12-02 12:55:28 -0800

    added time span slider

---
+++ analysis.r
@@ -0,0 +1,3 @@
+# read in meteorite landings dataset
+meteorite <- read.csv("./data/meteorite-landings.csv", stringsAsFactors = FALSE)
+
--- ui.R
+++ ui.R
@@ -1,28 +1,19 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Meteorite Landings"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      sliderInput("year.range",
+                  "Time Span (years)",
+                  min = 860,
+                  max = 2016,
+                  value = c(860, 2016))
     ),
     
     # Show a plot of the generated distribution
