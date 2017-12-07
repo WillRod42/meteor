6fb32c50fa624f06830d9a0e033bddcba9db62bb
Billy Wang <wangw38@uw.edu>
2017-12-06 22:52:25 -0800

    year graph

---
+++ chart_two.R
@@ -0,0 +1,33 @@
+# load required packages
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(tidyr)
+
+# set my working directory as needed
+#setwd("~/Desktop/meteor")
+
+#mass for meteorites are in grams
+
+# read in raw dataset
+dataset <- read.csv("./data/meteorite-landings.csv")
+
+# filter out incorrect data
+meteorite.data <- dataset %>%
+  filter(year>=860 & year<=2016) %>% # filter out weird years
+  filter(reclong<=180 & reclong>=-180 & (reclat!=0 | reclong!=0)) # filter out weird locations
+
+
+
+masses_byYear <- meteorite.data %>% 
+  select(mass, year) 
+
+#makes a color point map
+#graph just off mass and do a count for year 
+#Add data points to map with value affecting size
+masses_mapped <- ggplot(meteorite.dFrame) + 
+  geom_point(mapping = aes(x = year, y = mass, size=mass), colour="Deep Pink", 
+             fill="Pink",pch=21, size=5, alpha=I(0.7)) + 
+            scale_size_continuous(range = c(0, 60000000))
+
+
